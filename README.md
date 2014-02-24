a9390a5cfdc9717eeeeaf2963c63e925ae96f0aa
Dustin Dodson <dustinleedodson@gmail.com>
2014-02-24 12:19:56 -0900

    Edited README file to give a brief description of problem, and initialized the base program.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ HW3
 ===
 
 TDD Code Kata for the FizzBuzz problem.
+A program that prints the integers from 1 to 100,
+but for multiples of three print "Fizz" instead 
+of the number and for the multiples of five print
+"Buzz". For numbers which are multiples of both 
+three and five print "FizzBuzz".
---
+++ fizzbuzz.cpp
@@ -0,0 +1,15 @@
+//Dustin Dodson
+//CS372 HW3
+//FizzBuzz Code Kata
+
+#define CATCH_CONFIG_MAIN	//Define the main function to be used by CATCH
+#include "catch.hpp"		//Include CATCH testing framework
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+TEST_CASE("FizzBuzz values to be computed", "[fizzBuzz]")
+{
+	REQUIRE(true);
+}
