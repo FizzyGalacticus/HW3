a4a54fec7c2029df9653644ecb8f02e6bbe6cb01
Dustin Dodson <dustinleedodson@gmail.com>
2014-02-24 12:29:41 -0900

    Added function for determining FizzBuzz values, failing the first test case.

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -5,11 +5,17 @@
 #define CATCH_CONFIG_MAIN	//Define the main function to be used by CATCH
 #include "catch.hpp"		//Include CATCH testing framework
 
+//I-O stream for outputting information
 #include <iostream>
 using std::cout;
 using std::endl;
 
+const bool fizzBuzz(const int num)
+{
+	return false;
+}
+
 TEST_CASE("FizzBuzz values to be computed", "[fizzBuzz]")
 {
-	REQUIRE(true);
+	REQUIRE(fizzBuzz(0));
 }
